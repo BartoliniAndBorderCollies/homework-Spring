9882682ce6b65776627da9c4d1ef79028b0f3000
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-17 16:10:21 +0200

    delete of override methods

--- src/main/java/com/homework/homework/Spring/repository/TaskRepository.java
+++ src/main/java/com/homework/homework/Spring/repository/TaskRepository.java
@@ -8,9 +8,6 @@ import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
-    Task save(Task task);
     List<Task> findAll();
     Optional<Task> findTaskById(Long id);
-    void delete(Task task);
-
 }
