0f0e0bfbea4564f18f476887c09d4998e46b343f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-17 15:57:16 +0200

    add logic to service layer

---
+++ src/main/java/com/homework/homework/Spring/service/TaskService.java
@@ -0,0 +1,48 @@
+package com.homework.homework.Spring.service;
+
+import com.homework.homework.Spring.model.Task;
+import com.homework.homework.Spring.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task addTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findTaskById(id);
+    }
+
+    public Task updateTaskToBeCompleted (Long id) {
+        Optional<Task> optionalTask = taskRepository.findTaskById(id);
+        Task task = optionalTask.orElseThrow(() -> new IllegalArgumentException("nie ma takiego taska" + id));
+
+        task.setCompleted(true);
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask (Long id) {
+        Optional<Task> optionalTask = taskRepository.findTaskById(id);
+        Task task = optionalTask.orElseThrow(() -> new IllegalArgumentException("nie ma takiego taska" + id));
+
+        taskRepository.delete(task);
+    }
+
+
+
+}
