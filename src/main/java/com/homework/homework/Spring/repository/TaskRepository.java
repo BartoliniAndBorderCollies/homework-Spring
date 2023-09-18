ac79549cfd0dd18f312f1cf4826cc0c759520c9e
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-18 20:14:56 +0200

    delete of duplicated methods

--- src/main/java/com/homework/homework/Spring/repository/TaskRepository.java
+++ src/main/java/com/homework/homework/Spring/repository/TaskRepository.java
@@ -3,11 +3,6 @@ package com.homework.homework.Spring.repository;
 import com.homework.homework.Spring.model.Task;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
-    List<Task> findAll();
-    Optional<Task> findTaskById(Long id);
 }
