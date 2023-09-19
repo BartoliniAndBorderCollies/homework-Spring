d721162a62ca9272ee4a32a11a4650a82be13b85
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-19 11:06:55 +0200

    add Http methods with authentication() and CSRF disabled

--- src/main/java/com/homework/homework/Spring/config/SpringSecurity.java
+++ src/main/java/com/homework/homework/Spring/config/SpringSecurity.java
@@ -2,10 +2,13 @@ package com.homework.homework.Spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 
 @Configuration
@@ -15,14 +18,22 @@ public class SpringSecurity {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(request -> request
-                        .requestMatchers("/show/warehouse")
-                        .authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/tasks/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/tasks/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/tasks/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/tasks/**").authenticated()
                         .anyRequest()
                         .permitAll())
-                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults()) // basic auth
+                .csrf(AbstractHttpConfigurer::disable) // trzeba było dodać, bo bez tego postman mógł tylko Get methods
+//                .formLogin(Customizer.withDefaults()) //form auth
 
                 .build();
     }
+
+
+
+
 
 
 }
